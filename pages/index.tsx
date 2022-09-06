138910f27b47993926afd2a54b32cc15b6b59a2c
Felix Orinda <forinda82@gmail.com>
2022-09-06 09:11:41 +0300

    Make homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,31 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React from 'react'
-import { axiosQuery } from '@ui-services/api'
+// import { axiosQuery } from '@ui-services/api'
 import HomeFeaturedPost from '@ui-components/HomeFeaturedPost'
 import HomeNews from '@ui-components/HomeNews'
 
 export default function Home() {
   const [posts, setPosts] = React.useState<any[]>([])
 
-  
+
   const fetchPosts = async () => {
-    const response = await axiosQuery.get('')
-    setPosts(response.data)
-    console.log(response.data);
+    //   const response = await axiosQuery.get('')
+    //   setPosts(response.data)
+    //   console.log(response.data);
+    // }
+    // React.useEffect(() => {
+    //   fetchPosts()
+    // } , [])
   }
-  React.useEffect(() => {
-    fetchPosts()
-  } , [])
-  return (
-    <div>
-      <HomeFeaturedPost posts={posts!} />
-      <HomeNews  posts={posts}/>
-    </div>
-  );
-}
+    return (
+      <div>
+        {/* <HomeFeaturedPost posts={posts!} />
+      <HomeNews  posts={posts}/> */}
+        <div>
+          <h1>Hello from loans.ke</h1>
+        </div>
+      </div>
+    );
+  }
+
